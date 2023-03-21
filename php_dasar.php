7941a3c0f2d0556ae268fb676f36c5448d7e9f06
fauzansyauqi <fauzansyauqi00@gmail.com>
2023-03-21 16:48:25 +0700

    first commit

---
+++ php_dasar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+    echo "Hello World";
+    ?>
+    <h2>Mengunakan Variabel</h2>
+    <?php
+    $nim = "312110269";
+    $nama = 'fauzan syauqi';
+    echo "NIM : " . $nim . "<br>";
+    echo "Nama : $nama";
+    ?>
+</body>
+
+</html>
